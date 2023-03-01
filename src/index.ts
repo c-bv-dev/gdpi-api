b162df9520134d17835830bb6129bf006b48f194
C-bv <charles.bouveret.dev@gmail.com>
2023-03-01 11:24:48 +0100

    misc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import app from './app';
 import config from '@config/config';
 import logger from '@config/logger';
+import app from './app';
 
 const exitHandler = () => {
     if (!server) return process.exit(1);
